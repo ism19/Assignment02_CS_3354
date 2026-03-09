9ee12f79f8cb9b14e06a615a180a99ecb0799ba8
tashag2007 <natashagallegos@Natashas-MacBook-Pro.local>
2026-03-09 12:11:16 -0500

    Added Triangle class

---
+++ edu/txstate/assignment2/Triangle.java
@@ -0,0 +1,27 @@
+package edu.txstate.assignment2;
+
+public class Triangle implements Shape {
+
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    
+    public double getArea() {
+        return (base * height) / 2;
+    }
+
+    
+    public String getClassName() {
+        return "Triangle";
+    }
+
+    
+    public String toString() {
+        return "Triangle (b=" + base + ", h=" + height + ")";
+    }
+}
