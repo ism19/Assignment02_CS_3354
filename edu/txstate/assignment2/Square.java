f1ec9f483ab965aab718c221aafe234af468765d
Ismah Hassan <ismahiesha.hassan@gmail.com>
2026-03-10 19:27:04 -0500

    Removed getSide and setSide, not included in guidelines

--- edu/txstate/assignment2/Square.java
+++ edu/txstate/assignment2/Square.java
@@ -8,15 +8,6 @@ public class Square implements Shape {
         this.side = side;
     }
 
-    public double getSide() {
-        return side;
-    }
-
-    public void setSide(double side) {
-        this.side = side;
-    }
-
-    @Override
     public double getArea() {
         return side * side;
     }
